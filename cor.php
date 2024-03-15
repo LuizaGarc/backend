d8b9276d1d521857e71bc7ed4ba8dcfd4ca358ab
LuizaGarc <124835365+LuizaGarc@users.noreply.github.com>
2024-03-15 08:45:04 -0300

    Create cor.php

---
+++ cor.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Olá mundo</p>'; 
+    $cor1 = "Azul";
+    if ($cor1 == "Azul"){
+       echo "A cor é Azul";
+    } elseif ($cor == "Amarelo"){
+      echo "A cor é Amarelo";
+    } else {
+      echo "Outra cor";
+    }
+    ?>
+
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
