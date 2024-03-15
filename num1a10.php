c88a7986b338c2284a2edb9a3121a7d5d3b252f8
LuizaGarc <124835365+LuizaGarc@users.noreply.github.com>
2024-03-15 08:58:32 -0300

    Create num1a10.php

---
+++ num1a10.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Olá mundo</p>'; 
+    $i = 0;
+    for ($i=1; $i <=10; $i++){
+      echo "Volta de número $i <br>";
+    }
+    ?>
+
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
